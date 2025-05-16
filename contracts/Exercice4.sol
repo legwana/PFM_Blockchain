62320ad5d739db5d83fa5ee073dc7c77faf72565
Bressu <medelfineq@gmail.com>
2025-05-16 12:10:10 +0100

    Add Exercise 4 contract - check if number is positive

---
+++ contracts/Exercice4.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Exercice4 {
+    function isPositive(int _number) public pure returns (bool) {
+        return _number > 0;
+    }
+} 
