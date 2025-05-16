edb251cc916d5684127b73ae47bc62f92220462c
Bressu <medelfineq@gmail.com>
2025-05-16 12:10:21 +0100

    Add Exercise 5 contract - check if number is even

---
+++ contracts/Exercice5.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Exercice5 {
+    function isEven(uint _number) public pure returns (bool) {
+        return _number % 2 == 0;
+    }
+} 
